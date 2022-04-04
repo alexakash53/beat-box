c0d874c3ac253b29f6daff868f1a31a0a6e76ab1
alexakash53 <alexakash53@gmail.com>
2022-04-04 16:54:50 +0600

    blog section add

--- src/component/Banner/Banner.js
+++ src/component/Banner/Banner.js
@@ -6,7 +6,7 @@ const Banner = () => {
     return (
         <div className='banner-area container'>
             <div className="text-banner">
-                <h2>beat box awesome <br /> feel the sound</h2>
+                <h2>beat box awesome <br /><span>feel the sound</span></h2>
                 <p>Start with our picks for the best headphones below.<br/> These are the ones we recommend based on our testing <br/>and in-depth headphones reviews.</p>
                 <button>Headphone</button>
             </div>
--- src/component/Blog/Blog.js
+++ src/component/Blog/Blog.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './Blog.css'
 
 const Blog = () => {
     return (
@@ -8,7 +9,11 @@ const Blog = () => {
                 <p>The Context API is a React structure that enables you to exchange unique details and assists in solving prop-drilling from all levels of your application.</p>
             </div>
             <div className="medium-blog">
-                <h2>What is context API ?</h2>
+                <h2>What is Sementc Element ?</h2>
+                <p>Semantic Elements represent their meanings to both the developer and the browser. The tag in the code tells the developer and the browser what to do. It defines the different sections and layout of web pages, which makes HTML more easily understandable.In HTML, there are some semantic elements used to define when developing the frontend of the web pages.</p>
+            </div>
+            <div className="medium-blog">
+                <h2>inline inline-block difference ?</h2>
                 <p>The Context API is a React structure that enables you to exchange unique details and assists in solving prop-drilling from all levels of your application.</p>
             </div>
         </div>
