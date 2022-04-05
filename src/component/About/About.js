fca0840e32b153ba660d388361c2c7849436c38c
alexakash53 <alexakash53@gmail.com>
2022-04-05 17:29:47 +0600

    Blog curection

--- src/component/About/About.js
+++ src/component/About/About.js
@@ -12,6 +12,8 @@ const About = () => {
                 <p> We work tirelessly to source the world 's best audio products. We evaluate products for performance, quality and long-term customer satisfaction. Our goal is that you love your purchase from headphones.com for years, not just weeks. In a world bursting with product choices it'
                     s more important than ever to apply a ruthless filter to product selection.Everything in our store has met an extremely high bar of quality and long - term customer satisfaction.</p>
                 
+                <p>This article starts off the module with a good look at what accessibility is — this includes what groups of people we need to consider and why, what tools different people use to interact with the web, and how we can make accessibility part of our web development workflow.</p>
+                
                 <p> We work tirelessly to source the world 's best audio products. We evaluate products for performance, quality and long-term customer satisfaction.</p>
                 
             </div>
--- src/component/Blog/Blog.js
+++ src/component/Blog/Blog.js
@@ -6,15 +6,25 @@ const Blog = () => {
         <div className='blog-area container'>
             <div className="medium-blog">
                 <h2>What is context API ?</h2>
-                <p>The Context API is a React structure that enables you to exchange unique details and assists in solving prop-drilling from all levels of your application.</p>
+                <p> In React we usually send data from one component to another. In that case the data of one component has to be sent to another child component as props. Often the situation is that the child component is much lower than our main component where your data is. That means that data component has another child component, again that child component has another child component. There may be many children doing this. It can be said to be like digging up data. We usually use Redux in such situations or the situation may be more complicated than that. In that case we have a global status that any child can access later if they want, without having to pass all the components as props and take data.</p>
             </div>
             <div className="medium-blog">
                 <h2>What is Sementc Element ?</h2>
-                <p>Semantic Elements represent their meanings to both the developer and the browser. The tag in the code tells the developer and the browser what to do. It defines the different sections and layout of web pages, which makes HTML more easily understandable.In HTML, there are some semantic elements used to define when developing the frontend of the web pages.</p>
+                <p> HTML5 semantic elements support all modern browsers.Generally semantic tags means about to an element call it with it 's proper name. which induces with it'
+                s prime criteria.Able to define separate parts with it 's separate name which previous version of HTML use with classes. Possible combinations are included here. Tags define HTML elements for markups in terms of browser of modern style approach.</p>
             </div>
             <div className="medium-blog">
                 <h2>inline inline-block difference ?</h2>
-                <p>The Context API is a React structure that enables you to exchange unique details and assists in solving prop-drilling from all levels of your application.</p>
+                <h3>Inline</h3>
+                <ul>
+                    <li>In inlien we cannot use top and bottom margin.</li>
+                    <li> We cannot set it 's height and width.</li>
+                </ul>
+                <h3>Inline-block</h3>
+                <ul>
+                    <li> In inlien.block we can use top and bottom margin.</li>
+                    <li> We can set it 's height and width.</li>
+                </ul>
             </div>
         </div>
     );
